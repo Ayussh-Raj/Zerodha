99a0f9e65d7f1c4679551d8a098e92566433f6bb
Ayush Raj <163712520+Ayussh-Raj@users.noreply.github.com>
2025-03-25 19:22:59 +0530

    Add files via upload

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../assets/styles/Sidebar.css';
+import { useContext } from 'react';
+import {StockValue} from '../pages/Home'
+const Sidebar = () => {
+    const {stock,updateStock}=useContext(StockValue);
+    const data = [
+        { name: 'Apple.st' },
+        { name: 'Google' },
+        { name: 'Hdfc Bank' },
+        { name: 'HINDUSTANTIMES.NS' },
+        { name: 'IBN' },
+        { name: 'INFY' },
+        { name: 'ITC.NS' },
+        { name: 'RELIANCE.NS' },
+        { name: 'SBIN.NS' },
+        { name: 'TCS.NS' }
+    ];
+    const handleClick=(e)=>{
+        updateStock(e.target.id);
+    }
+    return (
+        <div className="sidebar">
+            <div className="search-bar">
+                <input type="text" placeholder="Search eg: infy bse, nifty fut, etc" />
+                <span className="counter">10/100</span>
+            </div>
+            <div className="stocks-list">
+                {data.map((item, index) => (
+                    <div className={`stock-item ${item.isPositive ? 'positive' : 'negative'}`} key={index} onClick={handleClick} id={item.name}>
+                        <span className="stock-name" id={item.name}>{item.name}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
---
+++ src/components/helpers/index.js
@@ -0,0 +1,15 @@
+
+import { API } from "../../core/backend";
+
+export const getMarketOverview = (token) => {
+  return fetch(`${API}/overview`, {
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((data) => {
+      return data.json();
+    })
+    .catch((err) => err);
+};
